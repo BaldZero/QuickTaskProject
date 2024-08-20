944bb0989bd0f7e6a75727d01e8f3a4c39eac771
Hyunmin Park <753429@student.dusd.net>
2024-08-20 15:28:33 -0700

    wadodds created
    coding stuff. added primitives

---
+++ QuickTaskWadOdds/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    private float horizontalInput
+    private float verticalInput
+    private float speed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vecctor2.right * horizontalInput * speed * Time.deltaTime); 
+    }
+}
