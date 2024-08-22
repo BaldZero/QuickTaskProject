b5388747d3cc38c2a9dc3ae1f667f9daaf5593ca
Hyunmin Park <753429@student.dusd.net>
2024-08-22 15:28:29 -0700

    flips sprite and stuff
    flips sprite and moves around properly. not jumping for some reason

--- QuickTaskWadOdds/Assets/Scripts/PlayerControl.cs
+++ QuickTaskWadOdds/Assets/Scripts/PlayerControl.cs
@@ -6,13 +6,22 @@ public class PlayerControl : MonoBehaviour
 {
     private float horizontalInput;
     private float verticalInput;
-    private Rigidbody2D playerRb;
     private float speed = 10f;
+    private float flip = -1;
+    public float jump = 10;
+
+    private Rigidbody2D playerRb;
+    
+    private bool isFacingRight;
+    private bool isOnGround;
+
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody2D>();
-    }
+        isFacingRight = true;
+        isOnGround = true;
+}
 
     // Update is called once per frame
     void Update()
@@ -20,10 +29,34 @@ public class PlayerControl : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
 
         playerRb.velocity = new Vector2(horizontalInput * speed, 0);
+
+        if(horizontalInput > 0 && !isFacingRight)
+        {
+            Flip();
+        }
+
+        if (horizontalInput < 0 && isFacingRight)
+        {
+            Flip(); 
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) )
+        {
+            playerRb.AddForce(Vector2.up * jump, ForceMode2D.Impulse);
+            isOnGround =false;  
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isOnGround = true; ;
     }
 
     private void Flip()
     {
+        Vector3 localScale = gameObject.transform.localScale;
+        localScale.x *= flip;
+        gameObject.transform.localScale = localScale;
+        isFacingRight = !isFacingRight; 
 
     }
      
